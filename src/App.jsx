34a59cb26ec6393297babdac64da3afc34d6a109
totodobronx <124589432+totodobronx@users.noreply.github.com>
2023-03-28 10:58:28 +0200

    PokéCard fait

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react';
+import PokemonCard from './components/PokemonCard';
+
+const App = () => {
+   return (
+     <div>
+       <PokemonCard />
+     </div>
+   );
+};
+
+export default App
