bf6ca19faed7c2d21abbf884292a285966dca7cb
totodobronx <124589432+totodobronx@users.noreply.github.com>
2023-03-29 14:55:06 +0200

    affichage ??? enfin fini

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 const PokemonCard = () => {
+  const pokemon = pokemonList[0]
   return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-      <figcaption>Bulbasaur</figcaption>
+    <figure className="card">
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} className="card-img"/>
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 };
