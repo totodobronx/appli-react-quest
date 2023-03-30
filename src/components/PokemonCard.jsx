df00a34821a3a762f8c9a9b59fd2771dd347d7b4
totodobronx <124589432+totodobronx@users.noreply.github.com>
2023-03-30 09:36:36 +0200

    correction faite

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -15,15 +15,4 @@ const PokemonCard = (props) => {
   );
 };
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
 export default PokemonCard;
